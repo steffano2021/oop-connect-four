5a6e5a2564125040baa43396fb191badb17680cd
Jun <luo.jnny@gmail.com>
2021-05-05 16:12:50 -0700

    column game.js

--- game.js
+++ game.js
@@ -1,19 +1,32 @@
+import{Column} from './column.js';
+
 export class Game {
-    constructor(playerOne, playerTwo, currentPlayer = 1){
+    constructor(playerOne, playerTwo){
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
-        this.currentPlayer = currentPlayer;
+        this.currentPlayer = 1;
+        this.columns = [new Column(), new Column(), new Column(), new Column(), new Column(), new Column(), new Column()]
     }
 
     getName(){
         return `${this.playerOne} vs. ${this.playerTwo}`
     }
 
-    playInColumn(){
+    playInColumn(columnIndex){
+
+       let currentColumn = this.columns[columnIndex]
+        currentColumn.add(this.currentPlayer);
+
         if (this.currentPlayer === 1){
             this.currentPlayer = 2;
         } else {
             this.currentPlayer = 1;
         }
     }
+
+    getTokenAt(rowIndex, columnIndex){
+        let currentColumn = this.columns[columnIndex];
+        return currentColumn.getTokenAt(rowIndex); //
+    }
+
 }
