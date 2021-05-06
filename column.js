b38c2fda76062f88a78098faf7fbf10940637db6
steffano <steffano.espinoza10@gmail.com>
2021-05-06 08:38:44 -0700

    debugged and works!

--- column.js
+++ column.js
@@ -6,18 +6,17 @@ export class Column{
 
     add(playerNum){
         if(this.tokens.length >= 6) return; //array could only have 6 indexes long
-        this.tokens.unshift(playerNum);
+        this.tokens.push(playerNum);
 
     }
 
     getTokenAt(num){ //can you give me back the value of the specific location
-        if(!this.tokens[num]){
+        if(!this.tokens[5-num]){
             return null
-        }else if(this.tokens[num] === 1){
+        }else if(this.tokens[5-num] === 1){
             return 1
-        }else if(this.tokens[num] === 2){
+        }else if(this.tokens[5-num] === 2){
             return 2
         }
-
     }
 }
