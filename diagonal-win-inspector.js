2217b606a8496045f1c476f16d9a86530f8afb10
Jun <luo.jnny@gmail.com>
2021-05-06 21:40:45 -0700

    diagonal complete

--- diagonal-win-inspector.js
+++ diagonal-win-inspector.js
@@ -15,21 +15,18 @@ export class DiagonalWinInspector {
                 }
             }
         }
-        console.log("hello")
-        for(let j = 3; j >= 0; j--) { //descending
-            console.log(j, "jay")
-            let valueAtColumn0 = this.columns[0].tokens[j+3]; //either 2 or 1
-            console.log(valueAtColumn0, "at 0")
-            let valueAtColumn1 = this.columns[1].tokens[j+2];
-            console.log(valueAtColumn1, "at 1")
-            let valueAtColumn2 = this.columns[2].tokens[j+1];
-            let valueAtColumn3 = this.columns[3].tokens[j];
 
-            if (valueAtColumn0 === valueAtColumn1 && valueAtColumn1 === valueAtColumn2 && valueAtColumn2 === valueAtColumn3){
-                if (valueAtColumn0 === undefined || valueAtColumn0 === null) {
+        for(let j = 0; j < 4; j++) { //descending
+            let bvalueAtColumn0 = this.columns[0].tokens[j+3]; //either 2 or 1
+            let bvalueAtColumn1 = this.columns[1].tokens[j+2];
+            let bvalueAtColumn2 = this.columns[2].tokens[j+1];
+            let bvalueAtColumn3 = this.columns[3].tokens[j];
+
+            if (bvalueAtColumn0 === bvalueAtColumn1 && bvalueAtColumn1 === bvalueAtColumn2 && bvalueAtColumn2 === bvalueAtColumn3){
+                if (bvalueAtColumn0 === undefined || bvalueAtColumn0 === null) {
                 return 0
             } else {
-                return valueAtColumn0
+                return bvalueAtColumn0
                 }
             }
         }
